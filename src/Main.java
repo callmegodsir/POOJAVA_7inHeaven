c179a9dbece58182662ba9c7ad64c11d9666581c
Pierre Chahine <pierrechahine2@gmail.com>
2025-04-20 22:43:44 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import view.LoginScreen;
 
 public class Main {
     public static void main(String[] args) {
-        // Créer et afficher l'écran de connexion, bonjour
+        // Créer et afficher l'écran de connexion, bonjou
         LoginScreen loginScreen = new LoginScreen();
         loginScreen.display();
     }
