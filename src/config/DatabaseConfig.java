e8d2f2248df5c80d8c86c8c758794fdb7b131ad7
Pierre Chahine <pierrechahine2@gmail.com>
2025-04-20 22:42:17 +0300

    commit

--- src/config/DatabaseConfig.java
+++ src/config/DatabaseConfig.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     // Modifiez cette ligne pour utiliser le port 3306 (port standard MySQL)
-    private static final String URL = "jdbc:mysql://localhost:8889/7inheaven?serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/7inheaven?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
